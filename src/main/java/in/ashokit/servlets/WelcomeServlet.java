56f7fd96dc5faf3c1d448dc4c4d7a6742b5f77bd
Abdul <shk.agaffar@gmail.com>
2025-03-02 06:52:12 +0530

    first commit

---
+++ src/main/java/in/ashokit/servlets/WelcomeServlet.java
@@ -0,0 +1,19 @@
+package in.ashokit.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value = { "/welcome" })
+public class WelcomeServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+}
